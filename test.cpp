77b3d2b64b298e46f325f51e3b5a696840c945ec
L_yuyou <1913438997@qq.com>
2024-02-26 22:43:27 +0800

    lq

--- test.cpp
+++ test.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+
+template <typename T>
+T max1(T a, T b) {
+    return (a > b) ? a : b;
+}
+
 int main()
 {
-    cout << endl;
-    cout << "hhhh"<<endl;
+    //int a = 1, b = 2, c = 3;
+    int ans = max1(1, 2);
+    cout << ans;
+    cout << "hh"<<endl;
     return 0;
 }
